51912c2264e4416a5f82b338edb9ffe03974f91b
unknown <shiyolinox@gmail.com>
2022-10-17 08:56:16 +0800

    hw

--- index.php
+++ index.php
@@ -4,17 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>基礎九九乘法表</title>
+    <title>hello world</title>
 </head>
 <body>
-<?php 
-for($j=1;$j<=9;$j++){
-    echo '<tr>';
-    for($i=1;$i<=9;$i++){
-        echo "<td> $j X $i =" . ($j*$i) . "</td>";
-     }
-    echo "</tr>";
-}
-?>
+<h1>hello world</h1>
 </body>
 </html>
